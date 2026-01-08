15dcb0700ab50166d48f2245adc8a6cc730e24c8
avfitnes <ashokvarma9636@gmail.com>
2026-01-08 18:46:06 +0530

    frontend setup  commit

--- src/services/google.service.js
+++ src/services/google.service.js
@@ -7,7 +7,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://resume-backend-s69p.onrender.com/api/auth/google",
+      callbackURL:
+        "https://resume-backend-s69p.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
